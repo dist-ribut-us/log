1db7a838f7c72c88d79b9811acf4c75fd1a482ec
Adam Colton <adamcolton@gmail.com>
2017-03-09 10:05:18 -0500

    Made Line signature insane.
    Line now accpets 0-3 arguments which do different things if they
    are positive or negative. But it ultimately simplifies things
    because the most common use-case now requries no arguments. Also,
    stack traces now omit the last few levels which are always the
    go runtime.

--- file.go
+++ file.go
@@ -41,37 +41,89 @@ func open(path ...string) (*os.File, error) {
 	return os.OpenFile(p, Contents|os.O_CREATE|os.O_WRONLY, 0666)
 }
 
-// Line attempts to return the file and line number i positions up the call
-// stack. It returns only the file name, not the directory. In the case that it
-// cannot fetch the information, an empty string is returned. If i is negative
-// it will return the whole stack
-func Line(i int) Val {
-	var file string
-	var l int
-	var ptr uintptr
-	ok := true
-	if i < 0 {
+// PathDepth sets how many directories to include when showing file names in
+// calls to Line
+var PathDepth = 1
+
+// Trim removes the bottom layers from the stack trace when calling Line. These
+// are always the runtime and so not useful information.
+var Trim uint = 2
+
+// Line can get either a single line or a stack trace. If no arguments are given
+// it will return the line from which it was invoked. If a positive int is given
+// it will return the line that many steps up the stack. If a negative int is
+// given, it will produce a stack trace starting that many steps up the stack.
+//
+// It also optionally takes a second int to set how many directory levels should
+// be included when showing file names. If this is negative, the entire path
+// will be included. If it is omitted, PathDepth will be used.
+//
+// A third argument will set the number of layers to trim from the bottom of the
+// stack. If it is negative it will print exactly that many layers. If it is
+// omitted, Trim will be used.
+//
+// tl;dr Just include log.Line() in a log call to include the file and line
+// number or put log.Line(-1) as the last argument a log call to include a stack
+// trace.
+func Line(ints ...int) Val {
+	l := len(ints)
+	i := 0
+	pd := PathDepth
+	trim := int(Trim)
+	if l > 0 {
+		i = ints[0]
+		if l > 1 {
+			pd = ints[1]
+			if i < 0 && l > 2 {
+				trim = ints[2]
+			}
+		}
+	}
+	if i < 0 { // return trace
 		strs := []string{""}
-		for i = -i; true; i++ {
-			ptr, file, l, ok = runtime.Caller(i)
+		for i = 1 - i; true; i++ {
+			str, ok := caller(i, pd)
 			if !ok {
 				break
 			}
-			strs = append(strs, fmt.Sprintf("%s:%d %s", getFileName(file), l, getFuncName(ptr)))
+			strs = append(strs, str)
 		}
-		return Lbl(strings.Join(strs, "\n  "))
-	}
-	ptr, file, l, ok = runtime.Caller(i + 1)
-	if !ok {
-		return Lbl("")
-	}
-	return Lbl(fmt.Sprintf("%s:%d %s", getFileName(file), l, getFuncName(ptr)))
+		if trim >= 0 {
+			l = len(strs) - trim - 1
+		} else {
+			l = len(strs) - 1
+			trim = -trim + 1
+			if trim < l {
+				l = trim
+			}
+		}
+		if l < 0 {
+			l = 0
+		}
+		return Lbl(strings.Join(strs[:l], "\n  "))
+	} // else return single line
+	str, _ := caller(i+2, pd)
+	return Lbl(str)
 }
 
-func getFileName(file string) string {
+func caller(i, pd int) (string, bool) {
+	ptr, file, ln, ok := runtime.Caller(i)
+	if !ok {
+		return "", false
+	}
+	return fmt.Sprintf("%s:%d %s", getFileName(file, pd), ln, getFuncName(ptr)), true
+}
+
+func getFileName(file string, pd int) string {
+	if pd < 0 {
+		return file
+	}
 	for i := len(file) - 1; i >= 0; i-- {
 		if file[i] == '/' {
-			return file[i+1:]
+			if pd == 0 {
+				return file[i+1:]
+			}
+			pd--
 		}
 	}
 	return file
